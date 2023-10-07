c688ee5a2ee3a58ea679cd6a4e404646d4a0025a
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-07 03:33:21 -0700

    t5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0(success)
+ */
+
+int main(void)
+{
+	int q;
+
+	for (q = 0; q < 10; q++)
+		printf("%d", q);
+	putchar ('\n');
+	return (0);
+}
