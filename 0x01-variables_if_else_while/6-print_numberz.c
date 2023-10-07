9c39e9de47a755775f1ff406ecdb781800655058
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-07 03:40:14 -0700

    t6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int q;
+
+	q = 0;
+	while
+		(q < 10) {
+			putchar (q + '0');
+			q++;
+		}
+	putchar('\n');
+	return (0);
+}
