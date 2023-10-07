5c8d840faeb545d65fa509988264d99f1c965b34
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-07 03:48:35 -0700

    t7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0 
+ */
+
+int main(void)
+{
+	char q;
+
+	q = 'z';
+	while
+		(q >= 'a') {
+			putchar(q);
+			q--;
+		}
+	putchar('\n');
+	return (0);
+}
