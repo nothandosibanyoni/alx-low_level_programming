b6adc9788639dfa7e8dc19e50ab700bb1caaa47d
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-07 03:59:26 -0700

    t8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0
+ */
+
+int main(void)
+{
+	char q;
+
+	int a;
+
+	q = 'a';
+	a =0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	while 
+		(q <= 'f') {
+			putchar(q);
+			q++;
+		}
+	putchar('\n');
+	return (0)
+}
