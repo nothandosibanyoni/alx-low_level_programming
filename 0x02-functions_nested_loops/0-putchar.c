0f5f7632c39659ebf2083fd65a8977e796a88079
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-10 13:10:33 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PRINT printf("_putchar\n")
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	if (PRINT)
+		n++
+	return (0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is  a README file for a 0x02 directory
