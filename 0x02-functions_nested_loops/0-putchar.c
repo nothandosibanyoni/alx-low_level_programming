d98fe4afd1e642e2232c3642c21654ecf7ddfffa
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-10 14:24:13 -0700

    t0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#ifndef main_h
-#define main_h
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - Entry point 
  * Return: always 0 (Success)
  */
 
 int main(void)
 {
 	printf("_putchar\n")
-		
-	return (0)
+		n++
+	return(0)
 }
