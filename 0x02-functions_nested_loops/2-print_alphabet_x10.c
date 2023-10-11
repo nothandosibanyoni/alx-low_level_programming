3782a6f68ef330bf6ad1052e3738527067dda02a
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-10 17:41:42 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,19 +4,19 @@
  * print_alphabet - create alphabet
  */
 
-void print_alphabet_x10(void)
-{
-char q;
-int a;
-a = 0
-	while (a < 10)
-{
-for (q = 'a'; q <= 'z'; q++)
-{
-_putchar(q);
-}
-_putchar('\n');
-a++;
-}
+void print_alphabet_x10(void);
+{		
+	char q;
+	int a;
 
+	a = 0
+	while (a < 10)
+
+	for (q = 'a'; q <= 'z'; q++)
+
+	_putchar(q);
+
+	_putchar('\n');
+	a++;
+	return (0);
 }
