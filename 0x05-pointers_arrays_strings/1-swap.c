2b82f54eb0495cc6b4cecbb6371bbf0bd83b5403
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-16 06:07:29 -0700

    swap1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two values of integers
+ * @a: swaps of integer
+ * @b: swaps of integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+}
