0b3a4d363e110810621ad0cc486ac643a2332ee1
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-16 06:41:41 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int string = 0;
+
+	while (*s != '\0')
+	{
+		string++;
+		s++;
+	}
+
+	return (string);
+}
