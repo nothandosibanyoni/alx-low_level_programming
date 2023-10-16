3b18b1d47a102e3fb71b40752e3fde78b931b05e
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-16 07:16:38 -0700

    printr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,24 @@
 /**
  * print_rev -  prints a string, in reverse, followed by a new line
  * @s: string to print reverse
+ * Return: 0
  */
 
 void print_rev(char *s)
 {
+	int string = 0;
+	int q;
+
 	while (*s != '\0')
 	{
-		_putchar(*s--);
+		string++;
+		s++;
+	}
+	s--;
+	for (q = string; q > 0; q--)
+	{
+		_putchar(*s);
+		s--;
 	}
 		_putchar('\n');
 }
