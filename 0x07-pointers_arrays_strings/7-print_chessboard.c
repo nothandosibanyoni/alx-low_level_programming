762e6affb26de2946458bb269b8ec114851299d6
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-23 14:18:08 -0700

    print

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_chessboard - function that locates a substring
+  * @a: function parameter declared
+  * Return: Always 0.
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int q;
+	int s;
+
+	for (q = 0; q < 8; q++)
+	{
+	for (s = 0; s < 8; s++)
+	_putchar(a[q][s]);
+	_putchar('\n');
+	}
+}
+
