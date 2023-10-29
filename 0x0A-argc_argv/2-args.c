b0d08396c82f70ac6cf946bc585db7fdf7f702c0
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-10-29 04:11:40 -0700

    2args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int q;
+
+	for (q = 0; q < argc; q++)
+
+	{
+
+		printf("%s\n", argv[q]);
+
+	}
+
+	return (0);
+}
