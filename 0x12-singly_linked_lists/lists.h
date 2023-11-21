422f6f101958a6bad9145af1693a6863dc84e70a
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-11-21 05:45:33 -0800

    printl

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
@@ -10,13 +13,12 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
