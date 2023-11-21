56aa2f0a5f210de707be6aa30fb6898935ee23d3
Nothando Sibanyoni <nothandosibanyoni97@gmail.com>
2023-11-21 14:06:46 -0800

    sumli

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int q = 0;
+	listint_t *s = head;
+
+	while (s)
+	{
+	q += s->n;
+	s = s->next;
+	}
+
+	return (q);
+}
